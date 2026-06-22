0fb2dfe1e5f6439d0ee8692def56232fd5c705f0
hatolife <poppo@hato.life>
2026-06-22 14:58:04 +0900

    fix(release): include signature files in zip

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -663,7 +663,7 @@ createApp({
         <section class="about-note">
           <h3>PGPで改竄確認できます</h3>
           <p>
-            GitHub Releasesでは、zipとは別に <code>ClipForVRChat.exe.asc</code> 署名ファイルも配布しています。
+            zipには <code>ClipForVRChat.exe.asc</code> 署名ファイルと公開鍵も入っています。GitHub Releasesでは、同じ署名ファイルと公開鍵をzipとは別にも配布しています。
           </p>
           <p>
             展開した <code>ClipForVRChat.exe</code> が改竄されていないことを確認できます。
@@ -671,8 +671,8 @@ createApp({
           <ol>
             <li><button class="link-button inline" @click="openURL(releasesUrl)">GitHub Releases</button> で使いたいバージョンを開きます。</li>
             <li>zipを展開し、確認したい <code>ClipForVRChat.exe</code> を用意します。</li>
-            <li>同じReleaseにある <code>ClipForVRChat.exe.asc</code> を、exeと同じフォルダに保存します。</li>
-            <li>公式の公開鍵を取り込みます。公開鍵は同じReleaseにある公開鍵ファイル、またはREADMEに記載されているものを使用してください。</li>
+            <li>zip内の <code>ClipForVRChat.exe.asc</code>、または同じReleaseに別添付されている <code>.exe.asc</code> を、exeと同じフォルダに保存します。</li>
+            <li>公式の公開鍵を取り込みます。公開鍵はzip内または同じReleaseにある公開鍵ファイルを使用してください。</li>
             <li>コマンドプロンプトやPowerShellで <code>gpg --verify ClipForVRChat.exe.asc ClipForVRChat.exe</code> を実行します。</li>
             <li><code>Good signature</code> と表示されれば、公式配布のexeとして確認できています。</li>
           </ol>
